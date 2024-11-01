a0ef55077560c9fda407d1dcfaf0e37b02c5ee68
Артём Лопатин <artt.lopatin2015@yandex.ru>
2024-11-01 14:00:42 +0300

    Добавьте файлы проекта.

---
+++ StackTemplate/StackTemplate.cpp
@@ -0,0 +1,1 @@
+#include "StackTemplate.h"
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
